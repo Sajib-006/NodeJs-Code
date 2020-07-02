09dfd7f77d095a94ac0bbf9a3e99405ad0316ee7
Sajib-006 <1605064@ugrad.cse.buet.ac.bd>
2020-07-03 03:31:20 +0600

    added write stream portion

--- streamread.js
+++ streamread.js
@@ -1,8 +1,12 @@
 const fs = require('fs');
 
 const rs = fs.createReadStream('./docs/blog3.txt',{encoding: 'utf-8'});
+const ws = fs.createWriteStream('./docs/blog4.txt');
 
 rs.on('data',(chunk) =>{
     console.log('.....NEW CHUNK.....');
     console.log(chunk);
+    ws.write('.....NEW CHUNK.....');
+    ws.write(chunk);
 })
+
