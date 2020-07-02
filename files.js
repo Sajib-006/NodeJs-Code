9db1778917cf4a401b68348e289cccc1bbd6d0f2
Sajib-006 <1605064@ugrad.cse.buet.ac.bd>
2020-07-02 23:30:21 +0600

    initial commit of files.js

---
+++ files.js
@@ -0,0 +1,32 @@
+const fs = require('fs');
+
+//reading files
+// fs.readFile('./docs/blog1.txt', (err, data) =>{
+//     if(err){
+//         console.log(err);
+//     }
+//     console.log(data);
+//     console.log(data.toString());
+
+// })
+
+console.log("hello"); //this will executed before showing file data 
+
+//writing files
+fs.writeFile('./docs/blog1.txt', 'hi again', ()=>{   //re-write blog1 file
+    console.log('file written');
+})
+
+fs.writeFile('./docs/blog2.txt', 'hi again', ()=>{   //create blog2 file
+    console.log('file written');
+})
+
+//directories
+if(!fs.existsSync('./dir1')){
+fs.mkdir('./dir1/', (err)=>{
+    if(err){
+        console.log(err);
+    }
+    console.log('folder created.');
+})
+}
