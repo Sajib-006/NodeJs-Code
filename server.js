b09aefe85cbddfd3da5079cda54f99b51f01a502
Sajib-006 <1605064@ugrad.cse.buet.ac.bd>
2020-07-03 11:36:23 +0600

    sending different html files

--- server.js
+++ server.js
@@ -1,14 +1,36 @@
 const http = require('http');
+const fs = require('fs');
 
 const server = http.createServer((req, res) =>{
     console.log('Request made.');
     console.log(req.url , req.method); //comma gives a space in-between parameters
     //console.log(res);
-    res.setHeader('Content-Type', 'text/html');
-    res.write('<head> <link rel="stylesheet" href="www.facebook.com"> </head>');
-    res.write('<p> Hello <a href="www.google.com"> google </a> </p>');
-    res.write('<p> My name is xyz.</p>');
-    res.end();
+     res.setHeader('Content-Type', 'text/html');
+    // res.write('<head> <link rel="stylesheet" href="www.facebook.com"> </head>');
+    // res.write('<p> Hello <a href="www.google.com"> google </a> </p>');
+    // res.write('<p> My name is xyz.</p>');
+    // res.end();
+    let path ='./views/';
+    switch(req.url){
+        case '/':
+            path += 'index.html';
+            break;
+        case '/about':
+            path += 'about.html';
+            break;
+        default:
+            path += '404.html';
+            break;
+    }
+    fs.readFile(path, (err,data) =>{
+        if(err){
+            console.log(err);
+            res.end();
+        }
+        res.write(data);
+        res.end();
+    })
+
 })
 
 server.listen(3000, 'localhost', () =>{
