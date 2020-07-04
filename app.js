113da4dfb63ae69d9baeee2e0ceefb305f7c2e0e
Sajib-006 <1605064@ugrad.cse.buet.ac.bd>
2020-07-05 01:49:41 +0600

    added ejs

--- app.js
+++ app.js
@@ -3,13 +3,17 @@ const express = require('express');
 //express app
 const app = express();
 
+//register view engine 
+app.set('view Engine', 'ejs');
+
 //listen for request
 app.listen(3000);
 
 //app will be executed top to bottom, so 404 page is at last when no expression will be executed.
 app.get('/', (req,res) =>{
     //res.send('<p> Home page </p>');   //send() doesn't need to set header like write()
-    res.sendFile('./views/index.html', { root: __dirname}); //here path is to be real path,so root is needed
+    //res.sendFile('./views/index.html', { root: __dirname}); //here path is to be real path,so root is needed
+    app.render('index2');
 })
 
 app.get('/about', (req,res) =>{
