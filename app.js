f73951271a40ca3ace1c3fed307b78d1cbb2b98a
Sajib-006 <1605064@ugrad.cse.buet.ac.bd>
2020-07-26 01:49:54 +0600

    connected to mongodb and added mongoose

--- app.js
+++ app.js
@@ -2,15 +2,23 @@ const express = require('express');
 // const ejsLint = require('ejs-lint');
 // ejsLint(text, options);
 const morgan = require('morgan');
+const mongoose = require('mongoose');
 
 //express app
 const app = express();
+
+//connect to mongodb
+const dbURI = 'mongodb+srv://dip:jvk2okhskEJpfopJ@nodejs.1k7mq.mongodb.net/NodeJS?retryWrites=true&w=majority';
+//using mongoose
+mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true})
+    .then((result)=> app.listen(4000))
+    .catch((err)=> console.log(err));
 
 //register view engine 
 app.set('view engine', 'ejs');
 
 //listen for request
-app.listen(4000);
+//app.listen(4000);
 
 //middleware & static files
 app.use(express.static('public'));
